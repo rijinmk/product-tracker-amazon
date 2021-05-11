0d92582e7066b5aa958721d7c3f612edff38d456
Rijin Mk <rijin.mk9@gmail.com>
2021-05-11 23:59:07 +0400

    Initial Files

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import time
+import requests
+import sys
+
+url = sys.argv[1]
+filename = sys.argv[2]
+print(url, filename)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+}
+
+f = open(filename + ".csv", "a")
+
+while True: 
+    html = requests.get(url, headers=headers)
+    html = html.text
+    soup = BeautifulSoup(html, "html.parser")
+    price = soup.find("span", {"id": "priceblock_ourprice"}).text.split("\xa0")[1]
+    print("Appending " + str(price) + " to file: " + filename)
+    f.write(str(int(time.time()))+"000" + ", " + price + "\n")
+    f.flush()
+    time.sleep(5)
